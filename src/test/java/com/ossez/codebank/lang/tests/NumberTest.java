7250836090204e3de46daa70bd1bc6cb9d4d5be5
YuCheng Hu <yuchenghu@ossez.com>
2018-12-30 17:08:37 -0500

    数字测试类，添加随机数测试函数

--- src/test/java/com/ossez/codebank/lang/tests/NumberTest.java
+++ src/test/java/com/ossez/codebank/lang/tests/NumberTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -26,5 +28,34 @@ public class NumberTest {
 	public void testRandom() {
 		logger.debug("{}", Math.random());
 		logger.debug("{}", Math.random());
+	}
+
+	/**
+	 * Random Interval from JDK
+	 */
+	@Test
+	public void testRandomInterval() {
+		logger.debug("0 <= R <= 10 - [{}]", (int) (Math.random() * 11));
+		logger.debug("5 <= R <= 10 - [{}] ", 5 + (int) (Math.random() * 6));
+
+		int min = 1;
+		int max = 100;
+		logger.debug("min <= R <= max - [{}] ", min + (int) (Math.random() * (max - min + 1)));
+	}
+
+	/**
+	 * Random Interval from JDK
+	 */
+	@Test
+	public void testRandomCommonsLang() {
+		int startInclusive = 1;
+		int endExclusive = 100;
+
+		logger.debug("min <= R <= max - [{}] ", RandomUtils.nextInt(startInclusive, endExclusive));
+		logger.debug("min <= R <= max - [{}] ", RandomUtils.nextFloat(startInclusive, endExclusive));
+		logger.debug("min <= R <= max - [{}] ", RandomUtils.nextDouble(startInclusive, endExclusive));
+
+		logger.debug("RandomStringUtils - [{}]", RandomStringUtils.randomAlphanumeric(10));
+
 	}
 }
